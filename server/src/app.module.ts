409cb47b99b6415f3f1d1a9f895a397a9c6512c8
Foks_f <107531680+Foksif@users.noreply.github.com>
2024-12-12 17:34:55 +0700

    add user & CRUD api

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from './prisma/prisma.module';
   imports: [UserModule, PrismaModule],
   controllers: [],
   providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+import { hashSync, genSaltSync } from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  save(user: Partial<User>) {
+    const hashedPassword = this.hashPassword(user.password);
+    return this.prisma.user.create({
+      data: {
+        email: user.email,
+        password: hashedPassword,
+        roles: ['USER'],
+      },
+    });
+  }
+
+  findOne(idOrEmail: string) {
+    return this.prisma.user.findFirst({
+      where: {
+        OR: [{ id: idOrEmail }, { email: idOrEmail }],
+      },
+    });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  delete(id: string) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+
+  private hashPassword(password: string) {
+    return hashSync(password, genSaltSync(10));
+  }
+}
